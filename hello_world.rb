e4321873f24560e57fa3f63c40ef24da6cf8d2dd
Henry G Sharpe <henrygsharpe@gmail.com>
2016-02-27 02:05:07 -0500

    Checkpoint 19 Address Bloc

---
+++ hello_world.rb
@@ -0,0 +1,10 @@
+def hello_world
+	
+	ARGV.each do |name|
+	  puts "Hello, #{name}!"
+	end
+
+	puts "Hello, World!"
+end
+
+hello_world
---
+++ spec/entry_spec.rb
@@ -0,0 +1,17 @@
+require_relative '../models/entry'
+
+RSpec.describe Entry do 
+
+	describe "attributes" do
+        
+	end
+
+	describe "#to_s" do
+        it "prints an entry as a string" do
+            entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+        	expected_string = "Name: Ada Lovelace\nPhone Number: 010.012.1815\nEmail: augusta.king@lovelace.com"
+
+        	expect(entry.to_s).to eq(expected_string)
+		end
+    end
+end
