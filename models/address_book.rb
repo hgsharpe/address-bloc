f0e1c65862a811bf4ee013006ff3ea090ed7b8db
hgsharpe <henrygsharpe@gmail.com>
2016-03-10 03:29:11 +0000

    Merge #3

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,5 @@
-require_relative "entry.rb"
+require_relative 'entry'
+require "csv"
 
 class AddressBook
   attr_accessor :entries
@@ -7,20 +8,30 @@ class AddressBook
     @entries = []
   end
 
-  def add_entry(name, phone, email)
-    
+  def remove_entry(name, phone_number, email)
+    delete_entry = nil
+  end
+
+  def add_entry(name, phone_number, email)
     index = 0
-    @entries.each do |entry|
-      if name < entry.name
-        break
-      end
-      index += 1
+    entries.each do |entry| 
+     if name < entry.name
+       break
+     end
+
+     index += 1
     end
 
-    @entries.insert(index,Entry.new(name,phone,email))
+    entries.insert(index, Entry.new(name, phone_number, email))
+  end
+
+  def import_from_csv(file_name)
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+    
+    csv.each do |row|
+      row_hash = row.to_hash
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
     end
- 
-  def remove_entry(email)
-    @entries.delete_if {|entry| entry.email == email}
   end
 end
